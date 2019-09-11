d727051e3854b16f5a75b654fdd6bcb153ba0ce7
TRONCY Damien <damien.troncy@sword-group.com>
2019-09-11 17:00:08 +0200

    Create go web server for interface

---
+++ interface/interface.go
@@ -0,0 +1,72 @@
+// Package main starts the simple server on port and serves HTML,
+// CSS, and JavaScript to clients.
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+var templates = template.Must(template.ParseFiles("index.html"))
+
+// logging is middleware for wrapping any handler we want to track response
+// times for and to see what resources are requested.
+func logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		req := fmt.Sprintf("%s %s", r.Method, r.URL)
+		log.Println(req)
+		next.ServeHTTP(w, r)
+		log.Println(req, "completed in", time.Now().Sub(start))
+	})
+}
+
+// index is the handler responsible for rending the index page for the site.
+func index() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		b := struct {
+			Title template.HTML
+		}{
+			Title: template.HTML("RC car command panel"),
+		}
+		err := templates.ExecuteTemplate(w, "index", &b)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("index: couldn't parse template: %v", err), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	})
+}
+
+// public serves static assets such as CSS and JavaScript to clients.
+func public() http.Handler {
+	return http.StripPrefix("/static/", http.FileServer(http.Dir("./static")))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/static/", logging(public()))
+	mux.Handle("/", logging(index()))
+
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "80"
+	}
+
+	addr := fmt.Sprintf(":%s", port)
+	server := http.Server{
+		Addr:         addr,
+		Handler:      mux,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+	log.Println("main: running simple server on port", port)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("main: couldn't start simple server: %v\n", err)
+	}
+}
